f8158c90bb398d9bd75fd0744577cff0dedecd52
jhadobe <jhoover@adobe.com>
2022-06-01 16:41:33 -0500

    Updating TOC file paths

--- src/data/navigation/sections/get_started.js
+++ src/data/navigation/sections/get_started.js
@@ -1,65 +1,65 @@
 module.exports = [
   {
-    title: "Introduction",
+    title: "Introduction/",
     path: "/get-started/",
   },
   {
-    title: 'Use REST APIs',
-    path: '/get-started/api-security',
+    title: "Use REST APIs/",
+    path: "/get-started/api-security/",
     header: true,
     pages: [
         {
-            title: 'API security',
-            path: '/get-started/api-security',
+            title: "API security/",
+            path: "/get-started/api-security/",
         },
         {
-            title: 'Construct a request',
-            path: '/get-started/gs-web-api-request'
+            title: "Construct a request/",
+            path: "/get-started/gs-web-api-request/",
         },
         {
-            title: 'Use cURL to run the request',
-            path: '/get-started/gs-curl'
+            title: "Use cURL to run the request/",
+            path: "/get-started/gs-curl"
         },
         {
-          title: 'Status codes and responses',
-          path: '/get-started/gs-web-api-response'
+          title: "Status codes and responses/",
+          path: "/get-started/gs-web-api-response/",
         }, 
     ],
 },
 {
-  title: "Use SOAP services",
+  title: "Use SOAP services/",
   path: "/get-started/soap-web-api-calls/",
 },
 {
-  title: 'Authentication',
-  path: '/get-started/authentication',
+  title: "Authentication/",
+  path: "/get-started/authentication/",
   header: true,
   pages: [
       {
-          title: 'Token-based authentication',
-          path: '/get-started/authentication/gs-authentication-token',
+          title: "Token-based authentication/",
+          path: "/get-started/authentication/gs-authentication-token/",
       },
       {
-        title: 'OAuth-based authentication',
-        path: '/get-started/authentication/gs-authentication-oauth',
+        title: "OAuth-based authentication/",
+        path: "/get-started/authentication/gs-authentication-oauth/",
       },
       {
-        title: 'OAuth error codes',
-        path: '/get-started/authentication/oauth-errors',
+        title: "OAuth error codes/",
+        path: "/get-started/authentication/oauth-errors/",
       
       },
       {
-        title: 'Session-based authentication',
-        path: '/get-started/authentication/gs-authentication-session',
+        title: "Session-based authentication/",
+        path: "/get-started/authentication/gs-authentication-session/",
       },
   ],
 },
 {
-  title: "Create an integration",
+  title: "Create an integration/",
   path: "/get-started/create-integration/",
 },
 {
-  title: "Web API functional testing",
+  title: "Web API functional testing/",
   path: "/get-started/web-api-functional-testing/",
 },
 ];
