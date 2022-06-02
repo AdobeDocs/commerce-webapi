95ff35dcce8582da894680108c125b72d511cba7
jhadobe <jhoover@adobe.com>
2022-06-02 17:46:39 -0500

    updating config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,5 +18,5 @@ module.exports = {
     subPages: subPages,
   },
   plugins: [`@adobe/gatsby-theme-aio`],
-  pathPrefix: process.env.PATH_PREFIX || '/commerce/services/'
+  pathPrefix: process.env.PATH_PREFIX || '/commerce/webapi/'
 };
