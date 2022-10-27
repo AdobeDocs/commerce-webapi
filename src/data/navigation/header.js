9f07d672e32db33ec04e9ef50242878e4c9060de
jhadobe <jhoover@adobe.com>
2022-10-27 18:07:53 -0500

    Modifying top nav

--- src/data/navigation/header.js
+++ src/data/navigation/header.js
@@ -11,6 +11,10 @@ module.exports = [
       title: "REST",
       path: "/rest/",
     },
+    {
+      title: "REST API Reference",
+      path: "/api-reference/",
+    },
     // {
     //   title: "GraphQL",
     //   path: "/graphql/",
---
+++ src/pages/api-reference.md
@@ -0,0 +1,7 @@
+---
+title: REST API reference
+description: Embedded ReDoc REST documentation
+frameSrc: https://good-stingray-87.redoc.ly
+--- 
+
+# REST endpoints (ReDoc)
