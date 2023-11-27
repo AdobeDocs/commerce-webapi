8c94bbaa10bb40d471894b6da09b593eb867db26
Dmytro Shevtsov <shevtsov@adobe.com>
2023-11-27 12:36:50 -0600

    Fix merging bug

--- .remarkrc.mjs
+++ .remarkrc.mjs
@@ -2,6 +2,7 @@ import remarkHeadingId from "remark-heading-id";
 import remarkValidateLinks from 'remark-validate-links';
 import remarkFrontmatter from 'remark-frontmatter';
 import remarkLintFrontmatterSchema from 'remark-lint-frontmatter-schema';
+import remarkLintNoDeadUrls from "remark-lint-no-dead-urls";
 
 const remarkConfig = {
 	plugins: [
