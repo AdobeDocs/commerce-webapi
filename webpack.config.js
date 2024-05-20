a91b5680ecf9b3d864943a62a6918037d531ac1f
timkim <timk@adobe.com>
2024-05-20 16:57:40 -0700

    Figuring out webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,29 @@
 const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin");
+let gatsbyConfig = require('./gatsby-config.js');
+
+function modify(buffer) {
+    // copy-webpack-plugin passes a buffer
+    var manifest = JSON.parse(buffer.toString());
+ 
+    // make any modifications you like, such as
+    //manifest.version = package.version;
+ 
+    // pretty print to JSON with two spaces
+    manifest_JSON = JSON.stringify(manifest, null, 2);
+    return manifest_JSON;
+ }
 
 module.exports = {
-  entry: './gatsby-config.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'nav.js',
-  },
+    entry: './gatsby-config.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'nav.js',
+      library: 'undefined',    // very important line
+      libraryTarget: 'window',    // very important line
+      umdNamedDefine: true 
+    },
+    plugins: [
+        
+    ]
 };
