21a25eefb0f9f931452d80cb8bb097140d7ecbe8
timkim <timk@adobe.com>
2024-05-21 14:58:04 -0700

    Setup scripts to build top and side nav

--- eds/scripts/buildNavigation.js
+++ eds/scripts/buildNavigation.js
@@ -1,3 +1,17 @@
 var nav = require('../out/gatsbyConfig.js');
+const path = require('path');
+const fs = require('node:fs');
+
 
 console.dir(nav, { depth: null, colors: true, maxArrayLength: null });
+
+
+try {
+  const topNav = JSON.stringify(nav.gatsbyConfig.siteMetadata.pages);
+  fs.writeFileSync(path.resolve(__dirname + '/../out/topNav.html'), topNav);
+
+  const sideNav = JSON.stringify(nav.gatsbyConfig.siteMetadata.subPages);
+  fs.writeFileSync(path.resolve(__dirname + '/../out/sideNav.html'), sideNav);
+} catch (err) {
+  console.error(err);
+}
