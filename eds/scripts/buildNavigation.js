6076d62c196c794203427acf5b896c5b50284072
timkim <timk@adobe.com>
2024-06-21 14:02:09 -0700

    Setup pointing to eds theme

--- eds/scripts/buildNavigation.js
+++ eds/scripts/buildNavigation.js
@@ -2,9 +2,7 @@ var nav = require('../out/gatsbyConfig.js');
 const path = require('path');
 const fs = require('node:fs');
 
-
 console.dir(nav, { depth: null, colors: true, maxArrayLength: null });
-
 
 try {
   const topNav = JSON.stringify(nav.gatsbyConfig.siteMetadata.pages);
