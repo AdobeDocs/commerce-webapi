995bf4cbde3fe43b3f085fd719b8019733340701
timkim <timk@adobe.com>
2024-06-24 15:13:07 -0700

    Get side and top nav script files

--- eds/scripts/buildNavigation.js
+++ eds/scripts/buildNavigation.js
@@ -2,14 +2,38 @@ var nav = require('../out/gatsbyConfig.js');
 const path = require('path');
 const fs = require('node:fs');
 
-console.dir(nav, { depth: null, colors: true, maxArrayLength: null });
-
 try {
-  const topNav = JSON.stringify(nav.gatsbyConfig.siteMetadata.pages);
-  fs.writeFileSync(path.resolve(__dirname + '/../out/topNav.html'), topNav);
+    const topNav = nav.gatsbyConfig.siteMetadata;
+    let topNavHtml = '';
 
-  const sideNav = JSON.stringify(nav.gatsbyConfig.siteMetadata.subPages);
-  fs.writeFileSync(path.resolve(__dirname + '/../out/sideNav.html'), sideNav);
+    // TODO: prob need url fixer from gatsby theme
+    if (topNav?.home) {
+        topNavHtml += `<li> <a href="${topNav.home.path}"> ${topNav.home.title} </a> </li>`;
+    }
+
+    topNav.pages?.forEach((navItem) => {
+        topNavHtml += `<li> <a href="${navItem.path}"> ${navItem.title} </a> </li>`;
+    });
+
+    fs.writeFileSync(path.resolve(__dirname + '/../out/topNav.html'), topNavHtml);
+
+    const sideNav = nav.gatsbyConfig.siteMetadata.subPages;
+    let sideNavHtml = ''
+    sideNavHtml = buildSideNavRecursively(sideNav);
+    fs.writeFileSync(path.resolve(__dirname + '/../out/sideNav.html'), sideNavHtml);
 } catch (err) {
-  console.error(err);
+    console.error(err);
+}
+
+function buildSideNavRecursively(sideNav) {
+    let sideNavHtml = '<ul>\n';
+    for (var k in sideNav) {
+        if (sideNav[k].pages) {
+            sideNavHtml += buildSideNavRecursively(sideNav[k].pages);
+        } else {
+            sideNavHtml += `<li> <a href="${sideNav[k].path}"> ${sideNav[k].title} </a> </li>\n`;
+        } 
+    }
+    sideNavHtml += '</ul>\n'
+    return sideNavHtml;
 }
