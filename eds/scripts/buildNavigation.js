5357f5e8a4020def80dc353c2c1d2d5c768a1d69
timkim <timk@adobe.com>
2024-08-06 00:25:01 -0700

    Make it md instead of html

--- eds/scripts/buildNavigation.js
+++ eds/scripts/buildNavigation.js
@@ -4,38 +4,64 @@ const fs = require('node:fs');
 
 try {
     const topNav = nav.gatsbyConfig.siteMetadata;
-    let topNavHtml = `<html>\n`;
+    const pathPrefix = nav.gatsbyConfig.pathPrefix;
+    console.log(nav.gatsbyConfig)
+    console.log('pathPrefix',pathPrefix)
+    let topNavMarkdown = ``;
 
     // TODO: prob need url fixer from gatsby theme
     if (topNav?.home) {
-        topNavHtml += `<li> <a href="${topNav.home.path}"> ${topNav.home.title} </a> </li>\n`;
+        
+        //topNavMarkdown += `<li> <a href="${topNav.home.path}"> ${topNav.home.title} </a> </li>\n`;
+        topNavMarkdown += `- [${topNav.home.title}](${topNav.home.path})\n`;
     }
 
     topNav.pages?.forEach((navItem) => {
-        topNavHtml += `<li> <a href="${navItem.path}"> ${navItem.title} </a> </li>\n`;
+        //topNavMarkdown += `<li> <a href="${navItem.path}"> ${navItem.title} </a> </li>\n`;
+        topNavMarkdown += `- [${navItem.title}](${navItem.path})\n`;
     });
 
-    topNavHtml += `</html>`
-    fs.writeFileSync(path.resolve(__dirname + '/../out/topNav.html'), topNavHtml);
+    fs.writeFileSync(path.resolve(__dirname + '/../out/topNav.md'), topNavMarkdown);
 
     const sideNav = nav.gatsbyConfig.siteMetadata.subPages;
-    let sideNavHtml = `<html>`;
-    sideNavHtml += buildSideNavRecursively(sideNav);
-    sideNavHtml += `</html>`;
-    fs.writeFileSync(path.resolve(__dirname + '/../out/sideNav.html'), sideNavHtml);
+    console.dir(sideNav)
+    let sideNavMarkdown = ``;
+    let depth = 0;
+    sideNavMarkdown += buildSideNavRecursively(sideNav, depth);
+    fs.writeFileSync(path.resolve(__dirname + '/../out/sideNav.md'), sideNavMarkdown);
 } catch (err) {
     console.error(err);
 }
+// subpages menu should only appear on the subpages path
+// need to check paths to
+function buildSideNavRecursively(sideNav, depth) {
+    let sideNavMarkdown = '';
 
-function buildSideNavRecursively(sideNav) {
-    let sideNavHtml = '<ul>\n';
     for (var k in sideNav) {
         if (sideNav[k].pages) {
-            sideNavHtml += buildSideNavRecursively(sideNav[k].pages);
+            sideNavMarkdown += buildSideNavRecursively(sideNav[k].pages, depth+1);
         } else {
-            sideNavHtml += `<li> <a href="${sideNav[k].path}"> ${sideNav[k].title} </a> </li>\n`;
+            //sideNavMarkdown += `<li> <a href="${sideNav[k].path}"> ${sideNav[k].title} </a> </li>`;
+            sideNavMarkdown += `${insertSpace(depth)}- [${sideNav[k].title}](${sideNav[k].path})\n`;
         } 
     }
-    sideNavHtml += '</ul>\n'
-    return sideNavHtml;
+    return sideNavMarkdown;
 }
+
+function insertSpace(indentLevel) {
+    let spaces = ``;
+    for(var i=0; i<indentLevel; i++){
+        spaces += `  `
+    }
+    return spaces;
+}
+
+// src/pages/topNav.md
+// src/pages/sideNav.md 
+// src/pages/get-started/sideNav.md
+
+// go through each subPages and find each path that relates to a subfolder 
+
+
+// title with path only
+// header setting
